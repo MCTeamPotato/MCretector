5011c44eb3a4dd1ec1cd558ace8b0c135da39c3c
Kasualix <670067575@qq.com>
2023-08-01 19:52:08 +0800

    import cleanup

--- src/main/java/com/teampotato/mcretector/MCretector.java
+++ src/main/java/com/teampotato/mcretector/MCretector.java
@@ -1,7 +1,5 @@
 package com.teampotato.mcretector;
 
-import com.teampotato.mcretector.mixin.MixinPlugin;
-import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.Mod;
 
 import static com.teampotato.mcretector.MCretector.MOD_ID;
