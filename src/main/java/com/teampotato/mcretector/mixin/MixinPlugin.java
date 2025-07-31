aee5552241dceb19d574273c604afdf2816f2f0e
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-07-31 12:09:27 +0800

    1.1.0

--- src/main/java/com/teampotato/mcretector/mixin/MixinPlugin.java
+++ src/main/java/com/teampotato/mcretector/mixin/MixinPlugin.java
@@ -1,9 +1,9 @@
 package com.teampotato.mcretector.mixin;
 
 import com.teampotato.mcretector.MCretector;
-import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraftforge.fml.loading.FMLLoader;
 import net.minecraftforge.fml.loading.moddiscovery.ModFileInfo;
+import net.minecraftforge.forgespi.language.IConfigurable;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
@@ -13,16 +13,18 @@ import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
 import java.io.IOException;
 import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Consumer;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 public class MixinPlugin implements IMixinConfigPlugin {
     public static final Logger LOGGER = LogManager.getLogger(MCretector.MOD_ID);
 
-    private static final List<String> MCREATOR_MODS = new ObjectArrayList<>();
-    private static final List<String> MODS_FAILED_TO_DETECT = new ObjectArrayList<>();
+    private static final Set<String> MCREATOR_MODS = new HashSet<>();
+    private static final Set<String> MODS_FAILED_TO_DETECT = new HashSet<>();
 
     public MixinPlugin() {
         LOGGER.info("MCreator mods detection starts.");
@@ -51,19 +53,29 @@ public class MixinPlugin implements IMixinConfigPlugin {
         List<ModFileInfo> modFileInfoList = FMLLoader.getLoadingModList().getModFiles();
         modFileInfoList.forEach(iModFileInfo -> {
             String jarFilePath = iModFileInfo.getFile().getFilePath().normalize().toString();
+            IConfigurable config = iModFileInfo.getConfig();
+            Consumer<Object> consumer = value -> {
+                if (value instanceof String && (((String) value).contains("mcreator") || ((String)value).contains("MCreator"))) MCREATOR_MODS.add(jarFilePath);
+            };
+            config.getConfigElement("credits").ifPresent(consumer);
+            config.getConfigElement("displayURL").ifPresent(consumer);
+            config.getConfigElement("authors").ifPresent(consumer);
+            config.getConfigElement("license").ifPresent(value -> {
+                if (value instanceof String && value.equals("Not specified")) MCREATOR_MODS.add(jarFilePath);
+            });
             try (JarFile jarFile = new JarFile(jarFilePath)) {
                 Enumeration<JarEntry> entries = jarFile.entries();
                 while (entries.hasMoreElements()) {
                     JarEntry entry = entries.nextElement();
                     String entryName = entry.getName();
 
-                    if (entry.isDirectory() && (entryName.contains("mcreator") || entryName.contains("procedures")) && !MCREATOR_MODS.contains(jarFilePath)) {
+                    if (entry.isDirectory() && (entryName.contains("mcreator") || entryName.contains("procedures"))) {
                         MCREATOR_MODS.add(jarFilePath);
                     }
                 }
             } catch (IOException exception) {
                 String modID = iModFileInfo.getFile().getFileName();
-                if (!MODS_FAILED_TO_DETECT.contains(modID)) MODS_FAILED_TO_DETECT.add(modID);
+                MODS_FAILED_TO_DETECT.add(modID);
             }
         });
         return modFileInfoList;
